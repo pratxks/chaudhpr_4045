6f4975a0af6bd791488e68716943699a6ea1ed16
pratxks <101081557+pratxks@users.noreply.github.com>
2023-02-06 21:06:42 +0530

    Initial Commit of Spring boot project

---
+++ src/main/java/com/pratikchaudhari/chaudhpr_4045/Chaudhpr4045Application.java
@@ -0,0 +1,13 @@
+package com.pratikchaudhari.chaudhpr_4045;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chaudhpr4045Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chaudhpr4045Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratikchaudhari/chaudhpr_4045/Chaudhpr4045ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratikchaudhari.chaudhpr_4045;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chaudhpr4045ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
