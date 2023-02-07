3f7ca3f4b3e49e5d9bbf971d2ff8e574d1368a5d
pratxks <101081557+pratxks@users.noreply.github.com>
2023-02-07 06:36:59 +0530

    Implemented Autowired and it works fine

--- src/main/java/com/pratikchaudhari/chaudhpr_4045/Chaudhpr4045Application.java
+++ src/main/java/com/pratikchaudhari/chaudhpr_4045/Chaudhpr4045Application.java
@@ -2,8 +2,12 @@ package com.pratikchaudhari.chaudhpr_4045;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.pratikchaudhari"})
+@ComponentScan(basePackages = {"com.pratikchaudhari.service"})
+
 public class Chaudhpr4045Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/pratikchaudhari/chaudhpr_4045/ChaudhprController.java
+++ src/main/java/com/pratikchaudhari/chaudhpr_4045/ChaudhprController.java
@@ -1,19 +1,62 @@
 package com.pratikchaudhari.chaudhpr_4045;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.pratikchaudhari.dto.SpecimenDTO;
+import com.pratikchaudhari.service.ISpecimenService;
+
 
 @Controller
 public class ChaudhprController {
+
+	@Autowired
+	private ISpecimenService specimenServiceStub;
 	
-	@RequestMapping("/start")
-	public String start() {
+	
+//	Handle the /index end point
+	
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+	public String read() {
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchById(36);
 		return "start";
 	}
 	
-	@RequestMapping("/")
-	public String index() {
+//	Read loyalty=blue
+	
+	@RequestMapping(value="/index", method=RequestMethod.GET, params= {"loyalty=blue"})
+	public String readBlue() {
 		return "start";
 	}
+	
+//	Read loyalty=silver
+	
+	@RequestMapping(value="/index", method=RequestMethod.GET, params= {"loyalty=silver"})
+	public String readSilver() {
+		return "start";
+	}
+	
+//	Read Header
+	
+	@RequestMapping(value="/index", method=RequestMethod.GET, headers= {"content-type=text/json"})
+	public String readJSON() {
+		return "start";
+	}
+	
+	@PostMapping(value="/index")
+	public String create() {
+		return "start";
+	}
+	
+//	Handle the / end point
+	
+//	@RequestMapping("/")
+//	public String index() {
+//		return "start";
+//	}
 
 }
